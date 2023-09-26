4fbd6bb1c7f9c40801d768a636608bd91bcd594f
Paula Pacheco <100471162+ennxxx@users.noreply.github.com>
2023-09-26 23:52:49 +0800

    updated so that row and col inputs are int; outputs are still floats

--- LBYARCH-S11-GRP-MachineProject01-PACHECOVILLACERAN.asm
+++ LBYARCH-S11-GRP-MachineProject01-PACHECOVILLACERAN.asm
@@ -4,6 +4,16 @@
 	ecall
 .end_macro
 
+.macro GET_INT(%x)
+       	li a7, 5
+	ecall
+.end_macro
+
+.macro GET_FLOAT(%x)
+    	li a7, 6
+    	ecall
+.end_macro
+
 .macro PRINT_DEC(%x)
        	li a7, 1
 	mv a0, %x
@@ -16,11 +26,6 @@
 	ecall
 .end_macro
 
-.macro GET_FLOAT(%x)
-    	li a7, 6
-    	ecall
-.end_macro
-
 .macro PRINT_FLOAT(%x)
 	fmv.s fa0, %x
 	li a7, 2
@@ -28,72 +33,54 @@
 .end_macro
 
 .data
-	empty: .float 0.0
-	counter: .float 1.0
 	input_row: .asciz "Row: "
 	input_col: .asciz "Column: "
 	input: .asciz "Input: "
 
 .text	
-	# for 1
-	li t5, 1
+	# initialize
+	li t1, 0 # row ctr
+	li t2, 0 # col 
+	li t3, 0 # reset
     	
     	# for row
     	PRINT_STRING input_row
-    	GET_FLOAT fa0
-    	fmv.s fa1, fa0 
+    	GET_INT a0
+    	mv a1, a0 
     	
     	# for column
 	PRINT_STRING input_col
-    	GET_FLOAT fa0
-    	fmv.s fa2, fa0
-    	
-    	# row counter 
-    	la t1, empty
-    	flw fa3, (t1)
-    	
-    	# col counter
-    	la t2, empty
-    	flw fa4, (t2)
-    	
-    	# counter
-    	la t3, counter
-    	flw fa5, (t3)
-    	
-    	# reset to 0
-    	la t4, empty
-    	flw fa6, (t4)
-    	
-    	# fa1 - row (ex.3)
-    	# fa2 - col (ex.2)
-    	# fa3 - row ctr
-    	# fa4 - col ctr
-    	# fa5 - increments 1
+    	GET_INT a0
+    	mv a2, a0
+    	    	    	    	
+    	# a1 - row 
+    	# a2 - col 
+    	# t1 - row ctr
+    	# t2 - col ctr
+    	# t3 - resets to 0
+    	# t4 - increments 1
     	
 	# iterate through columns for row num of times
 	
-	# compare rows
 	get_row:  
-	feq.s t4, fa1, fa3
-	beqz t4, get_col
-	beq t4, t5, add_row
+	bne a1, t1, get_col
+	beq a1, t1, add_row
 	
 	get_col:
 	PRINT_STRING input
 	GET_FLOAT fa0
-	fmv.s fa7, fa0
-	fadd.s fa3, fa3, fa5 # increment row
+	fmv.s fa1, fa0
+	addi t1, t1, 1
 	J get_row
 	
 	add_row: 
-	fadd.s fa4, fa4, fa5 # increment column
-	fmv.s fa3, fa6 # reset row counter to 0
-	feq.s t6, fa2, fa4
-	beqz t6, get_row
-	beq t6, t5, output
+	addi t2, t2, 1
+	mv t1, t3 
+	bne a2, t2, get_row
+	beq a2, t2, output
 	
 	output:
-	PRINT_FLOAT fa7 # print last number inputted
+	PRINT_FLOAT fa1 # print last number inputted
 	
 # September 24 (DONE)
 # step 1: maybe flw??
